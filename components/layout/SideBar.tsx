11d4b3cf23a62a16b8896f162596c21453e3424e
Nel-sokchhunly <nel.sokchhunly19@kit.edu.kh>
2023-06-19 10:37:44 +0700

    feat: slide active sidebar

--- components/layout/SideBar.tsx
+++ components/layout/SideBar.tsx
@@ -11,36 +11,63 @@ export default function SideBar({
   currentTab: HomePageTab;
   handlePageRouting: (tab: HomePageTab) => void;
 }) {
+  const handleTranslate = () => {
+    switch (currentTab) {
+      case HomePageTab.HOME:
+        return 'translate-y-0';
+      case HomePageTab.SAVED:
+        return 'translate-y-14';
+      case HomePageTab.REQUEST_STATUS:
+        return 'translate-y-28';
+      case HomePageTab.PROFILE:
+        return 'translate-y-[10.5rem]';
+    }
+  };
+
   return (
-    <div className='w-52 h-full px-[16px] space-y-4 hidden md:flex flex-col pb-10'>
-      <NavbarBtn
-        title='Home'
-        iconPath='/icon/sidenav/home.svg'
-        activeIconPath='/icon/sidenav/home-active.svg'
-        isCurrentTab={currentTab === HomePageTab.HOME}
-        onClick={() => handlePageRouting(HomePageTab.HOME)}
-      />
-      <NavbarBtn
-        title='Saved'
-        iconPath='/icon/sidenav/saved.svg'
-        activeIconPath='/icon/sidenav/saved-active.svg'
-        isCurrentTab={currentTab === HomePageTab.SAVED}
-        onClick={() => handlePageRouting(HomePageTab.SAVED)}
-      />
-      <NavbarBtn
-        title='Request'
-        iconPath='/icon/sidenav/request status.svg'
-        activeIconPath='/icon/sidenav/request status-active.svg'
-        isCurrentTab={currentTab === HomePageTab.REQUEST_STATUS}
-        onClick={() => handlePageRouting(HomePageTab.REQUEST_STATUS)}
-      />
-      <NavbarBtn
-        title='Profile'
-        iconPath='/icon/sidenav/profile.svg'
-        activeIconPath='/icon/sidenav/profile-active.svg'
-        isCurrentTab={currentTab === HomePageTab.PROFILE}
-        onClick={() => handlePageRouting(HomePageTab.PROFILE)}
-      />
+    <div className='w-52 h-full px-[16px] hidden md:flex flex-col pb-10'>
+      <div className='relative'>
+        <div className='z-10'>
+          <NavbarBtn
+            title='Home'
+            iconPath='/icon/sidenav/home.svg'
+            activeIconPath='/icon/sidenav/home-active.svg'
+            isCurrentTab={currentTab === HomePageTab.HOME}
+            onClick={() => handlePageRouting(HomePageTab.HOME)}
+          />
+          <NavbarBtn
+            title='Saved'
+            iconPath='/icon/sidenav/saved.svg'
+            activeIconPath='/icon/sidenav/saved-active.svg'
+            isCurrentTab={currentTab === HomePageTab.SAVED}
+            onClick={() => handlePageRouting(HomePageTab.SAVED)}
+          />
+          <NavbarBtn
+            title='Request'
+            iconPath='/icon/sidenav/request status.svg'
+            activeIconPath='/icon/sidenav/request status-active.svg'
+            isCurrentTab={currentTab === HomePageTab.REQUEST_STATUS}
+            onClick={() => handlePageRouting(HomePageTab.REQUEST_STATUS)}
+          />
+          <NavbarBtn
+            title='Profile'
+            iconPath='/icon/sidenav/profile.svg'
+            activeIconPath='/icon/sidenav/profile-active.svg'
+            isCurrentTab={currentTab === HomePageTab.PROFILE}
+            onClick={() => handlePageRouting(HomePageTab.PROFILE)}
+          />
+        </div>
+
+        {/* slide active button */}
+        <div
+          className={`
+            z-0 w-full h-14 rounded-xl bg-white
+            absolute top-0 right-0
+            transition-all duration-300
+            transform ${handleTranslate()}
+          `}
+        ></div>
+      </div>
 
       {/* admin */}
       <div className='flex-1'></div>
@@ -75,19 +102,19 @@ function NavbarBtn({
   return (
     <div
       className={`flex items-center justify-start cursor-pointer rounded-xl 
-      ${isCurrentTab ? 'bg-action text-primary' : 'text-[#D0B49F]'}
+      ${isCurrentTab ? ' text-primary' : 'text-[#D0B49F]'}
       p-2 px-4
-      transition-all
-      
+      transition-all 
+      h-14 z-10
       `}
       onClick={onClick}
     >
       <img
         src={isCurrentTab ? activeIconPath : iconPath}
         alt={title}
-        className='h-6'
+        className='h-6 z-10'
       />
-      <div className='ml-[12px] font-bold'>{title}</div>
+      <div className='ml-[12px] font-bold z-10'>{title}</div>
     </div>
   );
 }
